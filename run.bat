c3e33978c5e82dcaf9fd0d4ef477227bb5fe1dc7
Ravi Kumar Singh <ravi.kumar.singh@kex-ag.com>
2017-07-04 17:01:37 +0200

    update run script

--- run.bat
+++ run.bat
@@ -1,1 +1,1 @@
-mvn clean install -Dmaven.test.skip=true & java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5006  -Xms1024M -Xmx50072M -jar target/cimt-semantic-similarity-0.0.1-SNAPSHOT.jar 
+mvn clean install -Dmaven.test.skip=true & java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5006  -Xms1024M -Xmx50072M -jar target/simt-semantic-similarity-0.0.1-SNAPSHOT.jar
